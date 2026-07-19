fee376be86f0c803f17bec11e73a102ae3368f1c
trakeop517 <arturbaimov2gmail.com>
2026-07-19 12:48:11 +0300

    Добавлена вставка сразу множества объектов

--- app/repositories/event_repo.py
+++ app/repositories/event_repo.py
@@ -1,14 +1,14 @@
+from typing import List, Dict, Any
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.dialects.postgresql import insert  
 from app.models.event import EventModel
 from app.api.schemas import EventCreate
 
 class EventRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
+
     async def create_event(self, event_data: EventCreate) -> EventModel:
-        """
-        Создает и сохраняет новое событие в PostgreSQL.
-        """
         db_event = EventModel(
             source=event_data.source,
             external_id=event_data.external_id,
@@ -20,3 +20,10 @@ class EventRepository:
         await self.session.commit()
         await self.session.refresh(db_event)
         return db_event
+
+    async def create_many(self, events_list: List[Dict[str, Any]]) -> None:
+        if not events_list:
+            return
+        stmt = insert(EventModel).values(events_list)
+        stmt = stmt.on_conflict_do_nothing(index_elements=["external_id"])
+        await self.session.execute(stmt)
